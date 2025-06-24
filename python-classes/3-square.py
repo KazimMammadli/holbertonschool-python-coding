0fb7e09376ada31cd9dfd84198ba77ba83183f41
Kazim <11111@holbertonstudents.com>
2025-06-24 17:24:05 +0400

    3.task

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -7,12 +7,12 @@ class Square:
     def __init__(self, size=0):
         """Initializes size of the square"""
         self.__size = size
-        
+
     @property
     def size(self):
         """Retrieves the size of square"""
         return self.__size
-    
+
     @size.setter
     def size(self, value):
         """Sets the size of square if there is no exception"""
@@ -21,7 +21,7 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-        
+
     def area(self):
         """Returns the area of square"""
         return self.__size ** 2
