4811b02a60fb6aac18fa57b80e9c0ae55a541332
Kazim <11111@holbertonstudents.com>
2025-06-24 15:01:11 +0400

    1.task

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,4 +1,4 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 """This model is used to create Square class"""
 
 
@@ -6,9 +6,9 @@ class Square:
     """It defines a class with a private instance attribute"""
     def __init__(self, size=0):
         """Initializes the attribute and raise exception if needed"""
-        try:
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        else:
             self.__size = size
-        except TypeError:
-            print("size must be an integer")
-        except ValueError:
-            print("size must be >= 0")
